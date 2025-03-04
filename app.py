c12683aca8cf8b9dc77e0f6d9b80b75b40ac1f5d
ThanushaGali <gali.thanusha0012@gmail.com>
2025-03-04 16:51:33 +0530

    Fixed Nginx configuration

--- app.py
+++ app.py
@@ -58,3 +58,12 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+import os
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
